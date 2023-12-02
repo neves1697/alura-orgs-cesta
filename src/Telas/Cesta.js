e1454ca73a9cb1d901805ae83e45f2770686f1eb
neves1697 <neves1697@gmail.com>
2023-12-02 14:29:43 -0300

    Aprendendo como usar o comando (Dimensions.get('screen').width) para adquirir a largura da tela, inserindo imagens e estilizando os componentes da tela

--- src/Telas/Cesta.js
+++ src/Telas/Cesta.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Image, StyleSheet, Dimensions } from "react-native";
+import topo from '../../assets/topo.png';
 
+const width = Dimensions.get('screen').width; // pegando a largura da tela
 
 export default function Cesta() {
-    return(
-        <Text>Teste</Text>
-    );
+    return <>
+        <Image source={topo} style={estilos.topo} />
+        <Text style={estilos.titulo} >Detalhes da Cesta</Text>
+    </>
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        color: "white",
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: "bold",
+        padding: 16
+    }
+});
