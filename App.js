b44b270ea668a1c878b8506ce6f8cfbff82eaa6f
neves1697 <neves1697@gmail.com>
2023-12-04 22:37:22 -0300

    Adicionando a função de carregamento (Apploaging) para mostrar ao usuário que o app está carregando

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
 import Cesta from './src/Telas/Cesta/Components';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import mock from './src/Mocks/Cesta';
+import AppLoading from 'expo-app-loading';
 
 
 export default function App() {
@@ -12,7 +13,7 @@ export default function App() {
 
   if (!fontCarregada) {
     return (
-      <View />
+      <AppLoading />
     );
   }
 
