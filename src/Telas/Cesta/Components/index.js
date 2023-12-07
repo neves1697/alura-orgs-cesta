b8a22a61be21e76b977c1b747ba0a7f67fa2e0c6
neves1697 <neves1697@gmail.com>
2023-12-06 21:54:15 -0300

    Adicionado lista de itens e feito a estilização dos mesmos

--- src/Telas/Cesta/Components/index.js
+++ src/Telas/Cesta/Components/index.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import Detalhes from "./Detalhes";
 
 // importando assets personalizados
 
 import Texto from '../../../Components/Texto';
 import Topo from "../Components/Topo";
+import Itens from "./Itens";
 
-
-export default function Cesta({ topo, detalhes }) {
-    return <>
+export default function Cesta({ topo, detalhes, itens }) {
+    return <ScrollView>
         <Topo {...topo} />
 
         <View style={estilos.cesta} >
             <Detalhes {...detalhes} />
+            <Itens {...itens} />
         </View>
-    </>
+    </ScrollView>
 }
 
 const estilos = StyleSheet.create({
